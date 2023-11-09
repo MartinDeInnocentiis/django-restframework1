26fa301ebfc9c02861c936a3ac0eccd0cba90e9d
Martin De Innocentiis <martindeinnocentiis90@gmail.com>
2023-11-08 21:07:52 -0300

    Se agregan los atributos name a las urls de API views

--- ejercicios_practica/marvel/e_commerce/api/urls.py
+++ ejercicios_practica/marvel/e_commerce/api/urls.py
@@ -3,9 +3,9 @@ from e_commerce.api.views import *
 
 
 urlpatterns = [
-    path('comic-list/', comic_list_api_view),
-    path('comic-retrieve/', comic_retrieve_api_view),
-    path('comic-create/', comic_create_api_view),
-    path('comic-filter/', comic_list_filtered_api_view),
-    path('comic-premium/', comic_list_advanced_filter_api_view)
+    path('comic-list/', comic_list_api_view, name='comic_list_api_view'),
+    path('comic-retrieve/', comic_retrieve_api_view, name='comic_retrieve_api_view'),
+    path('comic-create/', comic_create_api_view, name='comic_create_api_view'),
+    path('comic-filter/', comic_list_filtered_api_view, name='comic_list_filtered_api_view'),
+    path('comic-premium/', comic_list_advanced_filter_api_view, name='comic_list_advanced_filter_api_view')
 ]
